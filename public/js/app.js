7afb4b69a3b44be5e97410711bebedc16988c345
maccana <mikeymccann@gmail.com>
2015-01-05 17:57:37 -0500

    Replaced app with newly generated yeoman meanjs

--- public/js/app.js
+++
@@ -1,5 +0,0 @@
-window.app = angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles']);
-
-angular.module('mean.system', []);
-angular.module('mean.articles', []);
-angular.module('mean.profiles', []);
